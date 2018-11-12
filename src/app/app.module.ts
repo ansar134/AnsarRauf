291e60d5bf1fa30cfccf60c0bef0bfff2ff7b5a3
ansar134 <ansarrauf@hotmail.com>
2018-11-12 21:33:27 +0800

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopnavbarComponent } from './components/topnavbar/topnavbar.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ExpertiseComponent } from './components/expertise/expertise.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopnavbarComponent,
+    ProjectsComponent,
+    ExperienceComponent,
+    EducationComponent,
+    ContactComponent,
+    SkillsComponent,
+    ExpertiseComponent
   ],
   imports: [
     BrowserModule
