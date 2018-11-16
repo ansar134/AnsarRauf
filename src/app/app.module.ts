1f24429dc744d92d4e06e1ccb097c06b78995208
Rauf <Ansar.Rauf@beumer.com>
2018-11-16 17:42:26 +0800

    work on the timeline feature and added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{Routes, RouterModule} from '@angular/router';
+import { VerticalTimelineModule } from 'angular-vertical-timeline';
 
 import { AppComponent } from './app.component';
 import { TopnavbarComponent } from './components/topnavbar/topnavbar.component';
@@ -10,6 +12,14 @@ import { ContactComponent } from './components/contact/contact.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ExpertiseComponent } from './components/expertise/expertise.component';
 
+const appRoutes: Routes=[
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'expertise', component: ExpertiseComponent },
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +32,9 @@ import { ExpertiseComponent } from './components/expertise/expertise.component';
     ExpertiseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    VerticalTimelineModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,17 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-
-
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
+
 export class ProjectsComponent implements OnInit {
-   prjs= 'src\assets\projects.json';
   
-  constructor() { }
+  prjs = {
+    "projects": [
+      {
+        "Title": "Title 1",
+        "Content": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam nam doloremque perspiciatis in ducimus fugiat autem praesentium quia consectetur quas sequi ea molestiae, recusandae et nemo voluptate ad magni voluptatem!"
+      },
+      {
+        "Title": "Title 2",
+        "Content": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam nam doloremque perspiciatis in ducimus fugiat autem praesentium quia consectetur quas sequi ea molestiae, recusandae et nemo voluptate ad magni voluptatem!"
+      },
+      {
+        "Title": "Title 3",
+        "Content": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam nam doloremque perspiciatis in ducimus fugiat autem praesentium quia consectetur quas sequi ea molestiae, recusandae et nemo voluptate ad magni voluptatem!"
+      },
+      {
+        "Title": "Title 4",
+        "Content": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam nam doloremque perspiciatis in ducimus fugiat autem praesentium quia consectetur quas sequi ea molestiae, recusandae et nemo voluptate ad magni voluptatem!"
+      },
+      {
+        "Title": "Title 5",
+        "Content": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam nam doloremque perspiciatis in ducimus fugiat autem praesentium quia consectetur quas sequi ea molestiae, recusandae et nemo voluptate ad magni voluptatem!"
+      },
+      {
+        "Title": "Title 6",
+        "Content": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam nam doloremque perspiciatis in ducimus fugiat autem praesentium quia consectetur quas sequi ea molestiae, recusandae et nemo voluptate ad magni voluptatem!"
+      },
+      {
+        "Title": "Title 7",
+        "Content": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam nam doloremque perspiciatis in ducimus fugiat autem praesentium quia consectetur quas sequi ea molestiae, recusandae et nemo voluptate ad magni voluptatem!"
+      }
+    ]
+  };
+  
+  constructor() { 
+   }
+   
 
   ngOnInit() {
+  }
+
+  projectIdxTrack(index, project)
+  {
+    return project.id;
   }
 
 }
